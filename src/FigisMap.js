f9644cfb4c9a1bfac719751635675eb4945fc594
Lorenzo Natali <offtherailz@gmail.com>
2012-11-25 00:40:40 +0100

    moved popup add in a separated function

--- src/FigisMap.js
+++ src/FigisMap.js
@@ -1147,6 +1147,160 @@ FigisMap.getStyleRuleDescription = function(STYLE, pars) {
 	if ( l == STYLE ) return '';
 	return l;
 }
+/**
+ * FigisMap.ol.clearPopupCache
+ * closes all popups, if any
+ */
+FigisMap.ol.clearPopupCache=function(){
+  if (FigisMap.popupCache){
+    for (var popupKey in FigisMap.popupCache){
+      var pu =FigisMap.popupCache[popupKey];
+      //close popups if opened
+      if (pu.opened){                 
+        FigisMap.popupCache[popupKey].close();
+      }
+      
+    }
+  }
+  //init
+  FigisMap.popupCache ={};  
+}
+/** 
+ * FigisMap.ol.createPopupControl(layers)
+ * create getFeatureInfo control to the map.
+ *
+ */
+FigisMap.ol.createPopupControl = function(vme){
+    FigisMap.ol.clearPopupCache();
+   
+		var info={controls : []};
+		var vmeLyr;
+		
+    var control;
+    for (var i = 0, len = info.controls.length; i < len; i++){
+        control = info.controls[i];
+        control.deactivate();  // TODO: remove when http://trac.openlayers.org/ticket/2130 is closed
+        control.destroy();
+	
+    }
+
+    for (vmeLyr=0; vmeLyr<vme.length; vmeLyr++){
+            
+      //VMSGetFeatureInfo FOR FIGIS-VME PROJECT
+      control = new OpenLayers.Control.WMSGetFeatureInfo({
+			  autoActivate: true,
+			  layers: [vme[vmeLyr]],
+			  queryVisible: true,
+			  maxFeatures: 10,
+			  //vendorParams: {"CQL_FILTER": "year = '" + Ext.getCmp('years-slider').getValues()[0] + "'"},
+			  eventListeners: {
+          beforegetfeatureinfo: function(e) { 
+            this.vendorParams = {"CQL_FILTER": "YEAR = '" + Ext.getCmp('years-slider').getValues()[0] + "'"};
+          }, 
+				  getfeatureinfo: function(e) {
+            var popupKey = e.xy.x + "." + e.xy.y;
+            var popup;
+            if (!(popupKey in FigisMap.popupCache)) {
+              popup = new GeoExt.Popup({
+						    title: 'Features Info',
+						    width: 400,
+						    height: 300,
+						    layout: "accordion",
+						    map: myMap,
+						    location: e.xy,
+						    listeners: {
+							    close: (function(key) {
+							        return function(panel){
+							            delete FigisMap.popupCache[key];
+							        };
+							    })(popupKey)
+						    }
+				      });
+					    FigisMap.ol.clearPopupCache();
+					    FigisMap.popupCache[popupKey] = popup;
+            }else{
+            	popup = FigisMap.popupCache[popupKey];
+            }
+
+            var addEncounters = function(btn){
+            	if (btn.pressed == true){		                   
+                myMap.getLayersByName('Encounters')[0].mergeNewParams({'CQL_FILTER': "YEAR = '" + Ext.getCmp('years-slider').getValues()[0] + "'"});
+                    myMap.getLayersByName('Encounters')[0].visibility = btn.pressed;
+                    myMap.getLayersByName('Encounters')[0].redraw(true);
+            	}else{
+                myMap.getLayersByName('Encounters')[0].mergeNewParams({'CQL_FILTER': "YEAR = '1000'"});
+                    myMap.getLayersByName('Encounters')[0].visibility = btn.pressed;
+            	myMap.getLayersByName('Encounters')[0].redraw(true);
+            	}
+            }
+            
+            var addServeyData = function(btn) {
+            	if (btn.pressed == true){			   
+                myMap.getLayersByName('SurveyData')[0].mergeNewParams({'CQL_FILTER': "YEAR = '" + Ext.getCmp('years-slider').getValues()[0] + "'"});
+                    myMap.getLayersByName('SurveyData')[0].visibility = btn.pressed;
+                    myMap.getLayersByName('SurveyData')[0].redraw(true);
+              }else{
+                  myMap.getLayersByName('SurveyData')[0].mergeNewParams({'CQL_FILTER': "YEAR = '1000'"});
+                      myMap.getLayersByName('SurveyData')[0].visibility = btn.pressed;
+                      myMap.getLayersByName('SurveyData')[0].redraw(true);
+              }
+            }
+            
+					  var buttonsVme = [];
+					
+					  if (e.object.layers[0].name == 'Established VME areas' && FigisMap.rnd.status.logged == true){
+						  buttonsVme = [
+							  {
+								  iconCls : 'encounters-icon',
+								  text    : 'Encounters',
+								  enableToggle: true,
+								  pressed : myMap.getLayersByName('Encounters')[0].visibility,
+								  handler : addEncounters
+							  },{
+								  iconCls : 'surveydata-icon',
+								  text    : 'Survey Data',
+								  enableToggle: true,
+								  pressed :myMap.getLayersByName('SurveyData')[0].visibility,
+								  handler : addServeyData
+							  }
+						  ]
+
+					  }
+					  var res = e.text.match(/<body[^>]*>([\s]*)<\/body>/);
+					
+					  e.object.layers[0].name
+					  if(!res){
+						  var oldItem;
+						  if (popup.items){
+							  oldItem =popup.items.get(e.object.layers[0].name);
+						  }
+						  if(oldItem){
+							  oldItem.update(e.text);
+						  }else{
+							  popup.add({
+								  itemId: e.object.layers[0].name,
+								  title: e.object.layers[0].name,
+								  layout: "fit",
+								  bodyStyle: 'padding:10px;background-color:#F5F5DC',
+								  html: e.text,
+								  autoScroll: true,
+								  autoWidth: true,
+								  collapsible: false,
+								  buttons : buttonsVme
+							  });
+							  popup.opened =true;
+							  popup.doLayout();
+							  popup.show();
+						  }
+					  }
+				  }
+			  }
+	    })  
+      info.controls.push(control);  
+    };
+    return info.controls;
+    
+}
 
 /*
 	Drawing function: FigisMap.draw( pars );
@@ -1229,6 +1383,8 @@ FigisMap.draw = function( pars ) {
 	
 	return FigisMap.lastMap;
 };
+
+
 
 FigisMap.renderer = function(options) {
 	var toBoundArray = new Array();
@@ -1417,164 +1573,13 @@ FigisMap.renderer = function(options) {
 
 			l.inMap = true;
 		}
-		FigisMap.popupCache = {};
-        
-		var vmeLyr;
 		
-        var control;
-        for (var i = 0, len = info.controls.length; i < len; i++){
-            control = info.controls[i];
-            control.deactivate();  // TODO: remove when http://trac.openlayers.org/ticket/2130 is closed
-            control.destroy();
-			document.getElementById("layerswitcher").innerHTML="";
-        }
-            
-		info.controls = [];
-		for (vmeLyr=0; vmeLyr<vme.length; vmeLyr++){
-            
-            //VMSGetFeatureInfo FOR FIGIS-VME PROJECT
-            control = new OpenLayers.Control.WMSGetFeatureInfo({
-					autoActivate: true,
-					layers: [vme[vmeLyr]],
-					queryVisible: true,
-					maxFeatures: 10,
-					//vendorParams: {"CQL_FILTER": "year = '" + Ext.getCmp('years-slider').getValues()[0] + "'"},
-					eventListeners: {
-                        beforegetfeatureinfo: function(e) { 
-                            this.vendorParams = {"CQL_FILTER": "YEAR = '" + Ext.getCmp('years-slider').getValues()[0] + "'"};
-                        }, 
-						getfeatureinfo: function(e) {
-		                    var popupKey = e.xy.x + "." + e.xy.y;
-		                    var popup;
-		                    if (!(popupKey in FigisMap.popupCache)) {
-				                popup = new GeoExt.Popup({
-									title: 'Features Info',
-									width: 400,
-									height: 300,
-									layout: "accordion",
-									map: myMap,
-									location: e.xy,
-									listeners: {
-										close: (function(key) {
-										    return function(panel){
-										        delete FigisMap.popupCache[key];
-										    };
-										})(popupKey)
-									}
-								});
-								for(var oldkey in FigisMap.popupCache){
-									FigisMap.popupCache[oldkey].close();
-								}
-								FigisMap.popupCache[popupKey] = popup;
-		                    } else{
-		                    	popup = FigisMap.popupCache[popupKey];
-		                    }
+    //document.getElementById("layerswitcher").innerHTML="";  
+		
 
-		                    var addEncounters = function(btn){
-		                    	if (btn.pressed == true){		                   
-				                    myMap.getLayersByName('Encounters')[0].mergeNewParams({'CQL_FILTER': "YEAR = '" + Ext.getCmp('years-slider').getValues()[0] + "'"});
-		                            myMap.getLayersByName('Encounters')[0].visibility = btn.pressed;
-		                            myMap.getLayersByName('Encounters')[0].redraw(true);
-		                    	}else{
-				                    myMap.getLayersByName('Encounters')[0].mergeNewParams({'CQL_FILTER': "YEAR = '1000'"});
-		                            myMap.getLayersByName('Encounters')[0].visibility = btn.pressed;
-				                	myMap.getLayersByName('Encounters')[0].redraw(true);
-		                    	}
-                            }
-		                    var addServeyData = function(btn) {
-		                    	if (btn.pressed == true){			   
-				                    myMap.getLayersByName('SurveyData')[0].mergeNewParams({'CQL_FILTER': "YEAR = '" + Ext.getCmp('years-slider').getValues()[0] + "'"});
-		                            myMap.getLayersByName('SurveyData')[0].visibility = btn.pressed;
-		                            myMap.getLayersByName('SurveyData')[0].redraw(true);
-				                }else{
-				                    myMap.getLayersByName('SurveyData')[0].mergeNewParams({'CQL_FILTER': "YEAR = '1000'"});
-		                            myMap.getLayersByName('SurveyData')[0].visibility = btn.pressed;
-		                            myMap.getLayersByName('SurveyData')[0].redraw(true);
-				                }
-				            }
-							var buttonsVme = [];
-							
-							if (e.object.layers[0].name == 'Established VME areas' && FigisMap.rnd.status.logged == true){
-								buttonsVme = [
-											{
-												iconCls : 'encounters-icon',
-												text    : 'Encounters',
-												enableToggle: true,
-												pressed : myMap.getLayersByName('Encounters')[0].visibility,
-												handler : addEncounters
-											},{
-												iconCls : 'surveydata-icon',
-												text    : 'Survey Data',
-												enableToggle: true,
-												pressed :myMap.getLayersByName('SurveyData')[0].visibility,
-												handler : addServeyData
-											}
-										]
-	
-							}
-							var res = e.text.match(/<body[^>]*>([\s]*)<\/body>/);
-							
-							e.object.layers[0].name
-							if(!res){
-								var oldItem;
-								if (popup.items){
-									oldItem =popup.items.get(e.object.layers[0].name);
-								}
-								if(oldItem){
-									oldItem.update(e.text);
-								}else{
-									popup.add({
-												itemId: e.object.layers[0].name,
-												title: e.object.layers[0].name,
-												layout: "fit",
-												bodyStyle: 'padding:10px;background-color:#F5F5DC',
-												html: e.text,
-												autoScroll: true,
-												autoWidth: true,
-												collapsible: false,
-												buttons : buttonsVme
-									});
-									
-									popup.doLayout();
-									popup.show();
-								}
-							}
-						}
-					}
-				})
-			//);
-			
-            myMap.addControl(control); 
-            info.controls.push(control);         
-		    //var panel = new OpenLayers.Control.Panel();
-		    
-            //panel.addControls([control]); 
-             
-            // add the panel to the map
-           // myMap.addControl(panel);
-        };		
-                        
-		function activateBtn(){
-	        for (var i = 0, len = info.controls.length; i < len; i++){
-	            control = info.controls[i];
-	            control.activate();
-	        }
-		}
+		var controls = FigisMap.ol.createPopupControl(vme);		
+    myMap.addControls(controls);                    
 		
-		function deactivateBtn(){
-	        for (var i = 0, len = info.controls.length; i < len; i++){
-	            control = info.controls[i];
-	            control.deactivate();
-				for(var oldkey in FigisMap.popupCache){
-					FigisMap.popupCache[oldkey].close();
-				}
-	        }
-		}
-		
-        // register events to the featureInfo tool
-        //control.events.register("activate", control, function() { activateBtn(); });                            
-        //control.events.register("deactivate", control, function() { deactivateBtn(); }); 
-            
 		FigisMap.debug( 'FigisMap.renderer layers array, after filling map:', layers );
 		
 		// BUILDING THE LEGEND
@@ -1756,10 +1761,8 @@ FigisMap.renderer = function(options) {
 } //FigisMap.renderer Class Ends
 Ext.onReady(function(){
 FigisMap.loginWin.on('login',function(user){
-            for (var popupKey in FigisMap.popupCache){                
-                FigisMap.popupCache[popupKey].close();
-            }            
-        });
+  FigisMap.ol.clearPopupCache();          
+});
 
 FigisMap.loginWin.on('logout',function(user){
 		for (var popupKey in FigisMap.popupCache){                
