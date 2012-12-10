b18304897ac82a2f30a1c1ccde40a7e2fab93d58
Riccardo Mari <riccardo_mari@tiscali.it>
2012-12-10 10:17:42 +0100

    update Openlayers Version 2.10

--- lib/OpenLayers/build/build.py
+++ lib/OpenLayers/build/build.py
@@ -3,62 +3,44 @@
 import sys
 sys.path.append("../tools")
 import mergejs
-import optparse
 
-def build(config_file = None, output_file = None, options = None):
-    have_compressor = []
+def build():
+    have_compressor = None
     try:
         import jsmin
-        have_compressor.append("jsmin")
+        have_compressor = "jsmin"
     except ImportError:
-        print "No jsmin"
-    try:
-        import closure
-        have_compressor.append("closure")
-    except Exception, E:
-        print "No closure (%s)" % E
-    try:
-        import closure_ws
-        have_compressor.append("closure_ws")
-    except ImportError:
-        print "No closure_ws"
-    
-    try:
-        import minimize
-        have_compressor.append("minimize")
-    except ImportError:
-        print "No minimize"
-
-    use_compressor = None
-    if options.compressor and options.compressor in have_compressor:
-        use_compressor = options.compressor
+        try:
+            import minimize
+            have_compressor = "minimize"
+        except Exception, E:
+            print E
+            pass
 
     sourceDirectory = "../lib"
     configFilename = "full.cfg"
     outputFilename = "OpenLayers.js"
 
-    if config_file:
-        configFilename = config_file
+    if len(sys.argv) > 1:
+        configFilename = sys.argv[1]
         extension = configFilename[-4:]
 
         if extension  != ".cfg":
-            configFilename = config_file + ".cfg"
+            configFilename = sys.argv[1] + ".cfg"
 
-    if output_file:
-        outputFilename = output_file
+    if len(sys.argv) > 2:
+        outputFilename = sys.argv[2]
 
     print "Merging libraries."
     merged = mergejs.run(sourceDirectory, None, configFilename)
-    print "Compressing using %s" % use_compressor
-    if use_compressor == "jsmin":
+    if have_compressor == "jsmin":
+        print "Compressing using jsmin."
         minimized = jsmin.jsmin(merged)
-    elif use_compressor == "minimize":
+    elif have_compressor == "minimize":
+        print "Compressing using minimize."
         minimized = minimize.minimize(merged)
-    elif use_compressor == "closure_ws":
-        minimized = closure_ws.minimize(merged)      
-    elif use_compressor == "closure":
-        minimized = closure.minimize(merged)      
     else: # fallback
+        print "Not compressing."
         minimized = merged 
     print "Adding license file."
     minimized = file("license.txt").read() + minimized
@@ -69,14 +51,4 @@ def build(config_file = None, output_file = None, options = None):
     print "Done."
 
 if __name__ == '__main__':
-  opt = optparse.OptionParser(usage="%s [options] [config_file] [output_file]\n  Default config_file is 'full.cfg', Default output_file is 'OpenLayers.js'")
-  opt.add_option("-c", "--compressor", dest="compressor", help="compression method: one of 'jsmin', 'minimize', 'closure_ws', 'closure', or 'none'", default="jsmin")
-  (options, args) = opt.parse_args()
-  if not len(args):
-    build(options=options)
-  elif len(args) == 1:
-    build(args[0], options=options)
-  elif len(args) == 2:
-    build(args[0], args[1], options=options)
-  else:
-    print "Wrong number of arguments"
+  build()
--- lib/OpenLayers/tools/release.sh
+++ lib/OpenLayers/tools/release.sh
@@ -2,39 +2,28 @@
 
 VERSION=$1
 
-wget -c http://closure-compiler.googlecode.com/files/compiler-latest.zip
-unzip compiler-latest.zip 
-
 svn export http://svn.openlayers.org/tags/openlayers/release-$VERSION OpenLayers-$VERSION
 cd OpenLayers-$VERSION/build
-mv ../../compiler.jar ../tools/closure-compiler.jar
-./build.py -c closure full
+./build.py full
 cp OpenLayers.js ..
-rm ../tools/closure-compiler.jar
 
 cd ..
-cd tools
-python exampleparser.py
-cd ..
-for i in google ie6-style style; do
-    csstidy theme/default/$i.css --template=highest theme/default/$i.tidy.css
-done    
 
 mkdir doc/devdocs
 mkdir doc/apidocs
 rm tools/*.pyc
 
-mkdir /osgeo/openlayers/docs/api/$VERSION
-cp OpenLayers.js /osgeo/openlayers/docs/api/$VERSION
-cp -a img/ /osgeo/openlayers/docs/api/$VERSION
-cp -a theme/ /osgeo/openlayers/docs/api/$VERSION
+mkdir /www/openlayers/htdocs/api/$VERSION
+cp OpenLayers.js /www/openlayers/htdocs/api/$VERSION
+cp -a img/ /www/openlayers/htdocs/api/$VERSION
+cp -a theme/ /www/openlayers/htdocs/api/$VERSION
 
 cd ..
 
-naturaldocs -i OpenLayers-$VERSION/lib -o HTML OpenLayers-$VERSION/doc/devdocs -p OpenLayers-$VERSION/doc_config -s Small OL
-naturaldocs -i OpenLayers-$VERSION/lib -o HTML OpenLayers-$VERSION/doc/apidocs -p OpenLayers-$VERSION/apidoc_config -s Small OL
+~/nd/NaturalDocs -i OpenLayers-$VERSION/lib -o HTML OpenLayers-$VERSION/doc/devdocs -p OpenLayers-$VERSION/doc_config -s Small OL
+~/nd/NaturalDocs -i OpenLayers-$VERSION/lib -o HTML OpenLayers-$VERSION/doc/apidocs -p OpenLayers-$VERSION/apidoc_config -s Small OL
 
 tar cvfz OpenLayers-$VERSION.tar.gz OpenLayers-$VERSION/
 zip -9r OpenLayers-$VERSION.zip OpenLayers-$VERSION/
 
-cp OpenLayers-$VERSION.* /osgeo/openlayers/docs/download 
+cp OpenLayers-$VERSION.* /www/openlayers/htdocs/download 
